aa6eb1bb67bba20407085c8b2509037b6f67370c
Niels <info@heimseiten.de>
2022-02-08 12:09:13 +0100

    changed columns name

--- src/Resources/contao/dca/tl_news.php
+++ src/Resources/contao/dca/tl_news.php
@@ -7,12 +7,12 @@ $GLOBALS['TL_DCA']['tl_news']['fields']['gallery'] = [
     'label'     => &$GLOBALS['TL_LANG']['tl_news']['gallery'], 
     'exclude'               => true,
     'inputType'             => 'fileTree',
-    'eval'                  => ['fieldType'=>'checkbox', 'files'=>true, 'multiple'=>true, 'tl_class'=>'clr', 'extensions'=>\Contao\Config::get('uploadTypes'), 'orderField'=>'orderImages', 'isGallery'=>true],
+    'eval'                  => ['fieldType'=>'checkbox', 'files'=>true, 'multiple'=>true, 'tl_class'=>'clr', 'extensions'=>\Contao\Config::get('uploadTypes'), 'orderField'=>'orderedImages', 'isGallery'=>true],
     'sql'                   => 'blob NULL'
 ];
 
-$GLOBALS['TL_DCA']['tl_news']['fields']['orderImages'] = [ 
-    'label'                 => &$GLOBALS['TL_LANG'][$table]['orderImages'],
+$GLOBALS['TL_DCA']['tl_news']['fields']['orderedImages'] = [ 
+    'label'                 => &$GLOBALS['TL_LANG'][$table]['orderedImages'],
     'sql'                   => 'blob NULL'
 ];
 
