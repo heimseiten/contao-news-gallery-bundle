c67eabad5109337d03853db35b77670ad3988df7
Niels <info@heimseiten.de>
2023-10-06 12:48:54 +0200

    Update tl_layout.php

--- src/Resources/contao/dca/tl_layout.php
+++ src/Resources/contao/dca/tl_layout.php
@@ -11,7 +11,10 @@ PaletteManipulator::create()
 $GLOBALS['TL_DCA']['tl_layout']['fields'] += [    
     'news_gallery_image_size_id' => [
         'inputType'        => 'imageSize',
-        'options'          => \System::getImageSizes(),
+        'options_callback' => static function ()
+			{
+				return Contao\System::getContainer()->get('contao.image.sizes')->getOptionsForUser(Contao\BackendUser::getInstance());
+			},
         'reference'        => &$GLOBALS['TL_LANG']['MSC'],
         'eval'             => array( 'rgxp' => 'digit', 'includeBlankOption' => true, 'tl_class'  => 'clr' ),
         'sql'              => "varchar(64) NOT NULL default ''"
